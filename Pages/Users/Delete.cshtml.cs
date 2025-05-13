b3011465c592db700a58919c8b01c72185d2c56a
Дима Будянский <budyanskiy1234@gmail.com>
2025-05-13 17:47:42 +0300

    Edit  + Delete

---
+++ Pages/Users/Delete.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using Casino_Project.Data;
+using Casino_Project.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Casino_Project.Pages.Users
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly AplicationDbContext _db;
+        [BindProperty]
+        public User User { get; set; }
+
+        public DeleteModel(AplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            User = _db.User.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var userFromDb = _db.User.Find(User.Id);
+            if(userFromDb != null)
+            {
+                _db.User.Remove(userFromDb);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }          
+            
+            return Page();
+        }
+    }
+}
---
+++ Pages/Users/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Casino_Project.Data;
+using Casino_Project.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Casino_Project.Pages.Users
+{
+    public class EditModel : PageModel
+    {
+        private readonly AplicationDbContext _db;
+        public User User { get; set; }
+
+        public EditModel(AplicationDbContext db)
+        {
+            _db = db;
+        }
+        public void OnGet(int id)
+        {
+            User = _db.User.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost(User user)
+        {
+            if (user.UserName == user.Balance.ToString())
+            {
+                ModelState.AddModelError("User.UserName", "Тест. Баланс не може дорівнювати юзернейму");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.User.Update(user);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
