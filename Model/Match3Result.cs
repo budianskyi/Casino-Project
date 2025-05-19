bc2a3f1e92da40ed4b7b925763de6b39baa83473
Nakovalnikov <LetTimMinekraft@gmail.com>
2025-05-20 00:22:46 +0300

    ъъ

---
+++ Model/Match3Result.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Casino_Project.Model;
+
+namespace Casino_Project.Model
+{
+    public class Match3Result  // ненужен
+    {
+        [Key]
+        public double BetAmount { get; set; }
+
+        [ForeignKey("User")]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
